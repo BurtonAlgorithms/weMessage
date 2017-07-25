fa4ab1473c4079511d57f0e08bdf7acf1ea302f6
RomanScott <romanscottpiano@gmail.com>
2017-07-25 18:36:29 -0500

    Renamed methodType to actionType for more clarity

--- commons/src/main/java/scott/wemessage/commons/json/action/JSONAction.java
+++ commons/src/main/java/scott/wemessage/commons/json/action/JSONAction.java
@@ -2,24 +2,24 @@ package scott.wemessage.commons.json.action;
 
 public class JSONAction {
 
-    private Integer methodType;
+    private Integer actionType;
     private String[] args;
 
-    public JSONAction(Integer methodType, String[] args){
-        this.methodType = methodType;
+    public JSONAction(Integer actionType, String[] args){
+        this.actionType = actionType;
         this.args = args;
     }
 
-    public Integer getMethodType() {
-        return methodType;
+    public Integer getActionType() {
+        return actionType;
     }
 
     public String[] getArgs() {
         return args;
     }
 
-    public void setMethodType(Integer methodType) {
-        this.methodType = methodType;
+    public void setActionType(Integer actionType) {
+        this.actionType = actionType;
     }
 
     public void setArgs(String[] args) {
