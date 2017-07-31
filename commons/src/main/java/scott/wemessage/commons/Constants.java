f23ee9612c3bdeb5515a0dfec9b2b5837888370f
RomanScott <romanscottpiano@gmail.com>
2017-07-31 15:48:36 -0500

    Changed Constants class to an interface

--- commons/src/main/java/scott/wemessage/commons/Constants.java
+++ commons/src/main/java/scott/wemessage/commons/Constants.java
@@ -1,52 +1,52 @@
 package scott.wemessage.commons;
 
-public abstract class Constants {
+public interface Constants {
 
-    public static final String WEMESSAGE_VERSION = "Alpha 0.1";
-    public static final int WEMESSAGE_BUILD_VERSION = 1;
-    public static final int DEFAULT_PORT = 22222;
-    public static final int MINIMUM_PASSWORD_LENGTH = 8;
-    public static final String DEFAULT_PASSWORD = "password";
+     String WEMESSAGE_VERSION = "Alpha 0.1";
+     int WEMESSAGE_BUILD_VERSION = 1;
+     int DEFAULT_PORT = 22222;
+     int MINIMUM_PASSWORD_LENGTH = 8;
+     String DEFAULT_PASSWORD = "password";
 
-    public static final String JSON_INIT_CONNECT = "INIT CONNECT - ";
-    public static final String JSON_VERIFY_PASSWORD_SECRET = "VERIFY PASSWORD SECRET - ";
-    public static final String JSON_SUCCESSFUL_CONNECTION = "SUCCESSFUL CONNECTION - ";
-    public static final String JSON_CONNECTION_TERMINATED = "CONNECTION TERMINATED - ";
-    public static final String JSON_NEW_MESSAGE = "NEW MESSAGE - ";
-    public static final String JSON_ACTION = "ACTION - ";
-    public static final String JSON_MESSAGE_UPDATED = "MESSAGE UPDATED - ";
-    public static final String JSON_RETURN_RESULT = "RETURN RESULT - ";
+     String JSON_INIT_CONNECT = "INIT CONNECT - ";
+     String JSON_VERIFY_PASSWORD_SECRET = "VERIFY PASSWORD SECRET - ";
+     String JSON_SUCCESSFUL_CONNECTION = "SUCCESSFUL CONNECTION - ";
+     String JSON_CONNECTION_TERMINATED = "CONNECTION TERMINATED - ";
+     String JSON_NEW_MESSAGE = "NEW MESSAGE - ";
+     String JSON_ACTION = "ACTION - ";
+     String JSON_MESSAGE_UPDATED = "MESSAGE UPDATED - ";
+     String JSON_RETURN_RESULT = "RETURN RESULT - ";
 
-    public static final int DISCONNECT_REASON_ALREADY_CONNECTED = 3000;
-    public static final int DISCONNECT_REASON_INVALID_LOGIN = 3001;
-    public static final int DISCONNECT_REASON_SERVER_CLOSED = 3002;
-    public static final int DISCONNECT_REASON_ERROR = 3003;
-    public static final int DISCONNECT_REASON_FORCED = 3004;
-    public static final int DISCONNECT_REASON_CLIENT_QUIT = 3005;
-    public static final int DISCONNECT_REASON_INCORRECT_VERSION = 3006;
+     int DISCONNECT_REASON_ALREADY_CONNECTED = 3000;
+     int DISCONNECT_REASON_INVALID_LOGIN = 3001;
+     int DISCONNECT_REASON_SERVER_CLOSED = 3002;
+     int DISCONNECT_REASON_ERROR = 3003;
+     int DISCONNECT_REASON_FORCED = 3004;
+     int DISCONNECT_REASON_CLIENT_QUIT = 3005;
+     int DISCONNECT_REASON_INCORRECT_VERSION = 3006;
 
-    public static final int ACTION_SEND_MESSAGE = 4000;
-    public static final int ACTION_SEND_GROUP_MESSAGE = 4001;
-    public static final int ACTION_RENAME_GROUP = 4002;
-    public static final int ACTION_ADD_PARTICIPANT = 4003;
-    public static final int ACTION_REMOVE_PARTICIPANT = 4004;
-    public static final int ACTION_LEAVE_GROUP = 4005;
-    public static final int ACTION_CREATE_GROUP = 4006;
+     int ACTION_SEND_MESSAGE = 4000;
+     int ACTION_SEND_GROUP_MESSAGE = 4001;
+     int ACTION_RENAME_GROUP = 4002;
+     int ACTION_ADD_PARTICIPANT = 4003;
+     int ACTION_REMOVE_PARTICIPANT = 4004;
+     int ACTION_LEAVE_GROUP = 4005;
+     int ACTION_CREATE_GROUP = 4006;
 
-    public static final int UNKNOWN_ERROR = 999;
-    public static final int SENT = 1000;
-    public static final int DELIVERED = 1001;
-    public static final int NO_INTERNET = 1002;
-    public static final int MESSAGE_SERVER_NOT_AVAILABLE = 1003;
-    public static final int INVALID_NUMBER = 1004;
-    public static final int NUMBER_NOT_IMESSAGE = 1005;
-    public static final int GROUP_CHAT_NOT_FOUND = 1006;
-    public static final int NOT_DELIVERED = 1007;
-    public static final int NOT_SENT = 1008;
-    public static final int SERVICE_NOT_AVAILABLE = 1009;
-    public static final int FILE_NOT_FOUND = 1010;
-    public static final int NULL_MESSAGE = 1011;
-    public static final int ASSISTIVE_ACCESS_DISABLED = 1012;
-    public static final int UI_ERROR = 1013;
-    public static final int ACTION_PERFORMED = 1014;
+     int UNKNOWN_ERROR = 999;
+     int SENT = 1000;
+     int DELIVERED = 1001;
+     int NO_INTERNET = 1002;
+     int MESSAGE_SERVER_NOT_AVAILABLE = 1003;
+     int INVALID_NUMBER = 1004;
+     int NUMBER_NOT_IMESSAGE = 1005;
+     int GROUP_CHAT_NOT_FOUND = 1006;
+     int NOT_DELIVERED = 1007;
+     int NOT_SENT = 1008;
+     int SERVICE_NOT_AVAILABLE = 1009;
+     int FILE_NOT_FOUND = 1010;
+     int NULL_MESSAGE = 1011;
+     int ASSISTIVE_ACCESS_DISABLED = 1012;
+     int UI_ERROR = 1013;
+     int ACTION_PERFORMED = 1014;
 }
