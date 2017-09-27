f4d6475e38450f1d95eb2c770051f8151bec4a60
RomanScott <romanscottpiano@gmail.com>
2017-09-26 20:59:26 -0500

    Added attachments information in notifications

--- commons/src/main/java/scott/wemessage/commons/connection/json/message/JSONNotification.java
+++ commons/src/main/java/scott/wemessage/commons/connection/json/message/JSONNotification.java
@@ -8,14 +8,16 @@ public class JSONNotification {
     private String handleId;
     private String chatId;
     private String chatName;
+    private String attachmentNumber;
 
-    public JSONNotification(String registrationToken, String encryptedText, String key, String handleId, String chatId, String chatName) {
+    public JSONNotification(String registrationToken, String encryptedText, String key, String handleId, String chatId, String chatName, String attachmentNumber) {
         this.registrationToken = registrationToken;
         this.encryptedText = encryptedText;
         this.key = key;
         this.handleId = handleId;
         this.chatId = chatId;
         this.chatName = chatName;
+        this.attachmentNumber = attachmentNumber;
     }
 
     public String getRegistrationToken() {
@@ -42,6 +44,10 @@ public class JSONNotification {
         return chatName;
     }
 
+    public String getAttachmentNumber() {
+        return attachmentNumber;
+    }
+
     public void setRegistrationToken(String registrationToken) {
         this.registrationToken = registrationToken;
     }
@@ -64,5 +70,9 @@ public class JSONNotification {
 
     public void setChatName(String chatName) {
         this.chatName = chatName;
+    }
+
+    public void setAttachmentNumber(String attachmentNumber) {
+        this.attachmentNumber = attachmentNumber;
     }
 }
